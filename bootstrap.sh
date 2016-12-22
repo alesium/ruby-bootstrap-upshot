17d89df273a75e9271d6fac3ab70ecb06f32c92e
Sebastien Perreault <sperreault@alesium.net>
2016-12-22 11:22:50 -0500

    fixed main.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,5 @@ BOOTSTRAP_URL="https://github.com/alesium/ruby-bootstrap-upshot.git"
 curl -s https://raw.githubusercontent.com/alesium/common-bootstrap-upshot/master/bootstrap.sh > /tmp/common-bootstrap.sh
 . /tmp/common-bootstrap.sh
 
-ansible-pull -d ${TEMPDIR} -i ${ANSIBLE_HOSTFILE} -e "ansible_python_interpreter=${PYTHONEXEC}" -U ${BOOTSTRAP_URL}
+ansible-pull -d ${TEMPDIR}/ruby-bootstrap-upshot -i ${ANSIBLE_HOSTFILE} -e "ansible_python_interpreter=${PYTHONEXEC}" -U ${BOOTSTRAP_URL}
+rm /tmp/common-bootstrap.sh
