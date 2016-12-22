caa456783760a2680fdbad566e7befe25b5bb3f4
Sebastien Perreault <sperreault@alesium.net>
2016-12-22 11:13:45 -0500

    give it a 4th try

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-set -x 
+set -x
 BOOTSTRAP_URL="https://github.com/alesium/ruby-bootstrap-upshot.git"
 
-. `curl -s https://raw.githubusercontent.com/alesium/common-bootstrap-upshot/master/bootstrap.sh | bash`
+curl -s https://raw.githubusercontent.com/alesium/common-bootstrap-upshot/master/bootstrap.sh > /tmp/common-bootstrap.sh
+. /tmp/common-bootstrap.sh
 
 ansible-pull -d ${TEMPDIR} -i ${ANSIBLE_HOSTFILE} -e "ansible_python_interpreter=${PYTHONEXEC}" -U ${BOOTSTRAP_URL}
